03574c57de85fdfe9d131cccdaaecb17512f0a19
9Mad-Max5 <maximilian.huebner95@gmx.de>
2019-12-24 15:08:44 +0100

    Update __init__.py

--- deezloader/__init__.py
+++ deezloader/__init__.py
@@ -117,7 +117,7 @@ class Login:
 						if a == "MP3_128":
 							raise exceptions.TrackNotFound("There isn't any quality avalaible for download this song: %s" % name)
 
-			name += " ({}){}".format(qualit, extension)
+			#name += " ({}){}".format(qualit, extension)
 
 			if os.path.isfile(name):
 				if recursive_download:
